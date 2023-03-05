5f0783d4579968ae3a0e1506c38e07ba9915bf01
FrequencyX4 <mrmichaelstollings@gmail.com>
2023-03-05 13:38:23 -0500

    feat: concept translator

--- translator.py
+++ translator.py
@@ -1,1 +1,23 @@
+from discord import app_commands
+from botutils import findall
 
+
+class Translator(app_commands.Translator):
+    translations = {}
+
+    async def load(self):
+        pass
+
+    async def translate(self, string: app_commands.local_str, locale: discord.Locale, ctx: app_commands.TranslationContext):
+        specials: list = await findall("\[.*]", string)
+        stripped = str(string)
+        for word in specials:
+            stripped = stripped.replace(word, "%s")
+
+        translation: str = self.translations[stripped]["the locale"]
+
+        if stripped in self.translations:
+            for i, word in enumerate(specials):
+                translation.replace("%s", word, 1)
+
+        return translation
